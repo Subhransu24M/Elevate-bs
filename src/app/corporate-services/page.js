053c042a72c4a1d477935701f72c779f2b23f5de
Ebs12345678 <subhransuseo18@gmail.com>
2024-04-15 18:08:01 +0530

    9th commit to main

--- src/app/corporate-services/page.js
+++ src/app/corporate-services/page.js
@@ -5,6 +5,11 @@ import TopBar from '../components/TopBar/TopBar';
 import CorporateContent from './CorporateContent';
 import './corporate.css';
 
+export const metadata = {
+  title: "Corporate Services |  Elevate Business Solutions",
+  description: "At Elevate, our team brings in ultimate client satisfaction by enabling them achieving their desired business goal.",
+  keywords: "corporate services in uae"
+};
 const page = () => {
   return (
     <>
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,10 +11,26 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
-
+      <head>
         <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></Script>
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></Script>
+
+        <meta name="google-site-verification" content="W8ujVfKx-JqFfGb4OK8BL3-wTAE2htqZf_BLygbMSps" />
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-NW0YT6F0RD"></Script>
+        <Script>{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-NW0YT6F0RD');
+        
+        `}
+
+        </Script>
+      </head>
+      <body className={inter.className}>
+        {children}
       </body>
     </html>
   );
