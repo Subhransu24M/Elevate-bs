9c555c7d8b3c5adfb708b06787d672d851bc466e
Ebs12345678 <subhransuseo18@gmail.com>
2024-04-15 18:50:19 +0530

    12th commit to main

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import Script from 'next/script';
 import "./globals.css";
 // import Link from 'next/link'
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -31,6 +31,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className={inter.className}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
