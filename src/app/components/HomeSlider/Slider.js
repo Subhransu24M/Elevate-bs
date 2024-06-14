af85477d41257b478d4d7265a5708710e9cab65d
Ebs12345678 <subhransuseo18@gmail.com>
2024-06-14 18:20:55 +0530

    48th commit to elevatebs main

--- src/app/components/HomeSlider/Slider.js
+++ src/app/components/HomeSlider/Slider.js
@@ -1,4 +1,5 @@
-import slider1 from '../../../../public/slider-banner/slider-1.webp'
+import slider1 from '../../../../public/slider-banner/slider-1.webp';
+import eidslider from './eidimage/eid-al-adha-2024.jpeg';
 import Image from 'next/image';
 import Link from 'next/link';
 // import {slidervideo} from '../../../../public/video/dubai.mp4'
@@ -7,7 +8,7 @@ import './slider.css';
 const Slider = () => {
     return (
         <>
-           {/* <div className='hm-slider-vdo'>
+            {/* <div className='hm-slider-vdo'>
            <video src={require('../../../../public/dubai.mp4')} autoPlay muted loop className="nextvideoshm" />
            </div> */}
 
@@ -19,14 +20,20 @@ const Slider = () => {
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div> */}
                 <div className="carousel-inner">
-                    <div className="carousel-item active sldr1">
-                    {/* <video src={require('../../../../public/dubai.mp4')} autoPlay muted loop className="nextvideoshm" />                       */}
-                         <Image src={slider1} className="d-block w-100" alt="..." height={550}/> 
+
+                <div className="carousel-item active">
+                        <Image src={eidslider} className="d-block w-100 sldimg" alt="eid al adha 2024" />
+                    </div>
+
+
+                    <div className="carousel-item">
+                        {/* <video src={require('../../../../public/dubai.mp4')} autoPlay muted loop className="nextvideoshm" />                       */}
+                        <Image src={slider1} className="d-block w-100 sldimg" alt="innovative support system" />
                         <div className="carousel-caption d-none d-md-block">
                             <h2 className='animationsldrtlt' data-aos="fade-up" data-aos-duration="800" data-aos-easing="linear" data-aos-delay="100"><span className='sl-hd-spn '>INNOVATIVE</span> SUPPORT SYSTEM</h2>
-                            <p className='animationsldrp'  data-aos="fade-up" data-aos-duration="800" data-aos-easing="linear" data-aos-delay="600">We provide innovative support system to any business.</p>
+                            <p className='animationsldrp' data-aos="fade-up" data-aos-duration="800" data-aos-easing="linear" data-aos-delay="600">We provide innovative support system to any business.</p>
                             <Link href="/contact-us" type="button" className="btn sld-cta-btn" data-aos="fade-zoom-in" data-aos-duration="800" data-aos-easing="linear" data-aos-delay="900">Contact us</Link>
-                        </div>    
+                        </div>
                     </div>
 
 
@@ -47,16 +54,16 @@ const Slider = () => {
                     </div> */}
 
 
-                    
+
                 </div>
-                {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Previous</span>
                 </button>
                 <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
-                </button> */}
+                </button>
             </div>
         </>
     )
