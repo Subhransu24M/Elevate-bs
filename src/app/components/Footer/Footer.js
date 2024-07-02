8dbeeb90811548566374c7a109fdc89c91390fd6
Ebs12345678 <subhransuseo18@gmail.com>
2024-07-02 13:53:09 +0530

    78th commit to elevatebs main

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -114,6 +114,7 @@ const Footer = () => {
                                         addressdata.map((val) => {
                                             return (
                                                 <button key="{id}"
+                                                
                                                     type="button"
                                                     className="btn btn-link ftr-adrs"
                                                     onClick={publish}
