2e43f80a41486d7f48e4a79d3b052c3b34b9d65a
Ebs12345678 <elevateds.03@gmail.com>
2024-11-25 13:03:21 +0530

    126th commit to elevatebs main

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -1,11 +1,11 @@
 import BreadCrumb from '../components/BreadCrumbs/BreadCrumb';
-import Footer from '../components/Footer/Footer';
+// import Footer from '../components/Footer/Footer';
 import FooterNew from '../components/Footer/FooterNew';
 
 
-import NavBar from '../components/NavBar/NavBar';
+// import NavBar from '../components/NavBar/NavBar';
 import NewNavbar from '../components/NavBar/NewNavbar';
-import TopBar from '../components/TopBar/TopBar';
+// import TopBar from '../components/TopBar/TopBar';
 // import Zohoform from '../components/Zohoform/Zohoform';
 import ContactContent from './ContactContent';
 import './contact.css';
