5aed61fe3e5b3535e9dcbf442c1b44a0edec37f5
Ebs12345678 <elevateds.03@gmail.com>
2024-11-30 18:48:20 +0530

    142th commit to elevatebs main

--- src/app/adgm/AdgmServices.js
+++ src/app/adgm/AdgmServices.js
@@ -29,17 +29,17 @@ const AdgmServices = () => {
                                         <p>{adgmservicelist.ShortDescription}</p>
                                     </div>
 
-                                    <div className='btn-blk-row'>
-                                        <div className='btn-blk'>
-                                            <button className='btn btn-danger bs-serv-btn-clr'><Link className="corp-serv-lrn-btn" href={adgmservicelist.PageLink}>Learn More</Link></button>
+                                    <div className='adgm-btn-blk-row'>
+                                        <div className='adgm-btn-blk'>
+                                            <button className='btn btn-danger adgm-bs-serv-btn-clr'><Link className="adgm-corp-serv-lrn-btn" href={adgmservicelist.PageLink}>Learn More</Link></button>
                                         </div>
-                                        <div className='action-btn-blk'>
-                                            <Link href="https://api.whatsapp.com/send?phone=971561672533" target="_blank" data-toggle="tooltip" data-placement="top" title="Chat with us"><Image className='action-icon' src={chaticon} data-aos="zoom-in" data-aos-easing="linear"
+                                        <div className='adgm-action-btn-blk'>
+                                            <Link href="https://api.whatsapp.com/send?phone=971561672533" target="_blank" data-toggle="tooltip" data-placement="top" title="Chat with us"><Image className='adgm-action-icon' src={chaticon} data-aos="zoom-in" data-aos-easing="linear"
                                                 data-aos-duration="3000" alt="icon" /></Link>
-                                            <Link href="tel:971561672533" data-toggle="tooltip" data-placement="top" title="Call us"><Image className='action-icon call-icon' src={callicon} data-aos="zoom-in" data-aos-easing="linear"
+                                            <Link href="tel:971561672533" data-toggle="tooltip" data-placement="top" title="Call us"><Image className='adgm-action-icon adgm-call-icon' src={callicon} data-aos="zoom-in" data-aos-easing="linear"
                                                 data-aos-duration="3000" alt="icon" /></Link>
 
-                                            <Link href="mailto:info@elevateauditing.com" data-toggle="tooltip" data-placement="top" title="Mail us"><Image className='action-icon gmail-icon' src={gmailicon} data-aos="zoom-in" data-aos-easing="linear"
+                                            <Link href="mailto:info@elevateauditing.com" data-toggle="tooltip" data-placement="top" title="Mail us"><Image className='adgm-action-icon adgm-gmail-icon' src={gmailicon} data-aos="zoom-in" data-aos-easing="linear"
                                                 data-aos-duration="3000" alt="icon" /></Link>
                                         </div>
                                     </div>
