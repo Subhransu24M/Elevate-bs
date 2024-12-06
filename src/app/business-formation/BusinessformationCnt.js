f5c290ffe0101402c91de5cfed7a48f5370d6f93
Ebs12345678 <elevateds.03@gmail.com>
2024-12-06 19:00:00 +0530

    154th commit to elevatebs main

--- src/app/business-formation/BusinessformationCnt.js
+++ src/app/business-formation/BusinessformationCnt.js
@@ -27,17 +27,17 @@ const BusinessformationCnt = () =>{
                                         <p>{bfdata.ShortDescription}</p>
                                     </div>
 
-                                    <div className='btn-blk-row'>
-                                        <div className='btn-blk'>
-                                            <button className='btn btn-danger bs-serv-btn-clr'><Link className="corp-serv-lrn-btn" href={bfdata.PageLink}>Learn More</Link></button>
+                                    <div className='bsf-btn-blk-row'>
+                                        <div className='bsf-btn-blk'>
+                                            <button className='btn btn-danger bsf-bs-serv-btn-clr'><Link className="bsf-corp-serv-lrn-btn" href={bfdata.PageLink}>Learn More</Link></button>
                                         </div>
-                                        <div className='action-btn-blk'>
-                                            <Link href="https://api.whatsapp.com/send?phone=971561672533" target="_blank" data-toggle="tooltip" data-placement="top" title="Chat with us"><Image className='action-icon' src={chaticon} data-aos="zoom-in" data-aos-easing="linear"
+                                        <div className='bsf-action-btn-blk'>
+                                            <Link href="https://api.whatsapp.com/send?phone=971561672533" target="_blank" data-toggle="tooltip" data-placement="top" title="Chat with us"><Image className='bsf-action-icon' src={chaticon} data-aos="zoom-in" data-aos-easing="linear"
                                                 data-aos-duration="3000" alt="icon" /></Link>
-                                            <Link href="tel:971561672533" data-toggle="tooltip" data-placement="top" title="Call us"><Image className='action-icon call-icon' src={callicon} data-aos="zoom-in" data-aos-easing="linear"
+                                            <Link href="tel:971561672533" data-toggle="tooltip" data-placement="top" title="Call us"><Image className='bsf-action-icon call-icon' src={callicon} data-aos="zoom-in" data-aos-easing="linear"
                                                 data-aos-duration="3000" alt="icon" /></Link>
 
-                                            <Link href="mailto:info@elevateauditing.com" data-toggle="tooltip" data-placement="top" title="Mail us"><Image className='action-icon gmail-icon' src={gmailicon} data-aos="zoom-in" data-aos-easing="linear"
+                                            <Link href="mailto:info@elevateauditing.com" data-toggle="tooltip" data-placement="top" title="Mail us"><Image className='bsf-action-icon gmail-icon' src={gmailicon} data-aos="zoom-in" data-aos-easing="linear"
                                                 data-aos-duration="3000" alt="icon" /></Link>
                                         </div>
                                     </div>
