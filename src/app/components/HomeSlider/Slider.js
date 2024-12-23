6f00bfd33673bc1e476c91d11ccf5280bc591b8e
Ebs12345678 <elevateds.03@gmail.com>
2024-12-23 12:17:57 +0530

    181th commit to elevatebs main

--- src/app/components/HomeSlider/Slider.js
+++ src/app/components/HomeSlider/Slider.js
@@ -25,11 +25,11 @@ const Slider = () => {
                     <Image src={diwalislider} className="d-block w-100 sldimg" alt="innovative support system" />
                     </div> */}
 
-                    <div className="carousel-item active" data-bs-interval="1000" >
+                    <div className="carousel-item active" data-bs-interval="2000" >
                     <Image src={christmasslider} className="d-block w-100 sldimg" alt="merry christmas 2025" />
                     </div>
 
-                    <div className="carousel-item" data-bs-interval="1000">
+                    <div className="carousel-item" data-bs-interval="3000">
                         <Image src={slider1} className="d-block w-100 sldimg" alt="innovative support system" priority/>
                         <div className="carousel-caption d-none d-md-block">
                             <h2 className='animationsldrtlt' data-aos="fade-up" data-aos-duration="800" data-aos-easing="linear" data-aos-delay="100"><span className='sl-hd-spn '>INNOVATIVE</span> SUPPORT SYSTEM</h2>
